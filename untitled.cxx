9c12f84beea74033bfb65c59a6b60b39743e0ad6
GraciaRosalinaHindirwan <145007784+GraciaRosalinaHindirwan@users.noreply.github.com>
2025-07-21 11:26:30 +0700

    Add files via upload

---
+++ untitled.cxx
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+void jumlah() {
+	cout << "Jumlahnya adalah...." << endl;
+}
+
+
+
+int main(int argc, char **argv)
+{
+	cout << "hello world" << endl;
+	jumlah();
+	
+	return 0;
+}
+
